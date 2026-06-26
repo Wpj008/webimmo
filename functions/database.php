c08292b4d910ea621e2a2d224dd9801d71564192
WPJ008 <wawitephilemon@gmail.com>
2026-06-26 17:25:14 +0200

    function database

---
+++ functions/database.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+function getPDO() { 
+    try{
+        $db =  new PDO('mysql:host=localhost;dbname=webimmo', "root", "");
+        return $db;
+    }
+    catch(PDOException $err){
+        var_dump($err);
+        throw $err;
+
+    }
+    
+}
+
+
+function debugPDO(PDO $db){
+    var_dump($db);
+}
+
+?>
